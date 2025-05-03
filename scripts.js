2781292ea7b35a826db7e68d93d6a8266976c88e
Neal Wu <neal@nealwu.com>
2025-05-03 11:27:27 -0700

    Replace with the new website

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const text =
+  "Sequence Holdings is a long-term holding company that partners with exceptional businesses and management teams, providing capital, operational expertise, and technology to drive sustainable growth and industry leadership. We are a small team based in NYC, founded by Neal Wu, Alex Rubin and Michael Lee.";
+const typingSpeed = 15;
+
+const typewriterText = document.getElementById("typewriter-text");
+const cursor = document.getElementById("typewriter-cursor");
+
+let index = 0;
+
+function type() {
+  if (index < text.length) {
+    typewriterText.textContent += text.charAt(index);
+    index++;
+    setTimeout(type, typingSpeed);
+  } else {
+    const button = document.querySelector(".button");
+    button.style.visibility = "visible";
+
+    setTimeout(() => {
+      button.style.opacity = "1";
+    }, 50);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(type, 1000);
+});
